d3ac284ab703af0b7b8effac21ccc12f8ea57a25
Pedro Perafan Carrasco <pedroperafan18@gmail.com>
2015-04-22 21:27:56 -0500

    Primer Push
    Pirmer Push

---
+++ src/concurso/InicioController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concurso;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author pedro
+ */
+public class InicioController implements Initializable {
+    
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
